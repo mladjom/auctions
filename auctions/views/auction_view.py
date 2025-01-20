f6fa7532eed8ed687fdc7a4879c69ff131b5b5fd
mladjom <mladenmilentijevic@gmail.com>
2025-01-20 10:39:00 +0100

    Detail View

--- auctions/views/auction_view.py
+++ auctions/views/auction_view.py
@@ -160,59 +160,128 @@ class AuctionListView(BaseListView):
 
 @method_decorator(cache_control(public=True, max_age=3600), name='dispatch')
 class AuctionDetailView(BaseDetailView):
-    """Detail view for auctions with multilingual support"""
+    """Detail view for displaying auction information with related auctions and navigation."""
     model = Auction
     template_name = 'auctions/auction_detail.html'
-    context_object_name = 'auction'
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
-
+    
     def get_queryset(self):
-        """Get queryset with related objects"""
+        """Optimize queryset with required related fields."""
         return (super().get_queryset()
                 .select_related('category', 'location', 'executor')
-                .prefetch_related('images', 'documents', 'tags'))
-
+                .filter(end_time__gt=timezone.now()))
+    
+    def get_navigation_objects(self):
+        """Get previous and next auctions within the same category."""
+        auction = self.object
+        base_query = self.get_queryset().filter(category=auction.category)
+        
+        prev_obj = base_query.filter(
+            end_time__lt=auction.end_time
+        ).order_by('-end_time').first()
+        
+        next_obj = base_query.filter(
+            end_time__gt=auction.end_time
+        ).order_by('end_time').first()
+        
+        return prev_obj, next_obj
+    
+    def get_related_objects(self, limit=3):
+        """Get related auctions from the same category."""
+        return (self.get_queryset()
+                .filter(category=self.object.category)
+                .exclude(pk=self.object.pk)
+                .order_by('?')[:limit])
+    
+    def get_meta_data(self):
+        """Get meta information for SEO."""
+        auction = self.object
+        return {
+            'meta_title': f"{auction.title} | {_('Auctions')}",
+            'meta_description': auction.description[:160]
+        }
+    
     def get_context_data(self, **kwargs):
-        """Get context data with additional auction information"""
+        """Extend context with auction-specific data."""
         context = super().get_context_data(**kwargs)
-        
-        # Get related auctions from same category
-        related_auctions = (
-            Auction.objects.filter(
-                #category=self.object.category,
-                status='CONFIRMED',
-                #end_time__gt=timezone.now()
-            )
-            .exclude(pk=self.object.pk)
-            .order_by('-start_time')[:3]
-        )
-
-        context.update({
-            'related_auctions': related_auctions,
-            'meta_title': self.object.meta_title,
-            'meta_description': self.object.meta_description,
-            'og_type': 'product',
-            'og_title': self.object.meta_title,
-            'og_description': self.object.meta_description,
-        })
-
+        context.update(self.get_meta_data())
         return context
-
+    
     def get_breadcrumbs(self):
-        """Get breadcrumbs for auction detail"""
-        breadcrumbs = super().get_breadcrumbs()
+        """Generate breadcrumb navigation for auction detail page."""
+        auction = self.object
+        return [
+            *super().get_breadcrumbs(),
+            {
+                'title': _('Auctions'),
+                'url': self.get_language_specific_url('auctions:auction-list')
+            },
+            # {
+            #     'title': auction.category.title,
+            #     'url': self.get_language_specific_url(
+            #         'auctions:category-detail',
+            #         kwargs={'slug': auction.category.slug}
+            #     )
+            # },
+            {
+                'title': auction.title,
+                'url': None
+            }
+        ]
+
+
+
+# @method_decorator(cache_control(public=True, max_age=3600), name='dispatch')
+# class AuctionDetailView(BaseDetailView):
+#     """Detail view for auctions with multilingual support"""
+#     model = Auction
+#     template_name = 'auctions/auction_detail.html'
+
+#     def get_queryset(self):
+#         """Get queryset with related objects"""
+#         return (super().get_queryset()
+#                 .select_related('category', 'location', 'executor')
+#                 .prefetch_related('images', 'documents', 'tags'))
+
+#     def get_context_data(self, **kwargs):
+#         """Get context data with additional auction information"""
+#         context = super().get_context_data(**kwargs)
         
-        # Add Auctions level
-        breadcrumbs.append({
-            'title': _('Auctions'),
-            'url': self.get_language_specific_url('auctions:auction-list')
-        })
+#         # Get related auctions from same category
+#         related_auctions = (
+#             Auction.objects.filter(
+#                 #category=self.object.category,
+#                 status='CONFIRMED',
+#                 #end_time__gt=timezone.now()
+#             )
+#             .exclude(pk=self.object.pk)
+#             .order_by('-start_time')[:3]
+#         )
+
+#         context.update({
+#             'related_auctions': related_auctions,
+#             'meta_title': self.object.meta_title,
+#             'meta_description': self.object.meta_description,
+#             'og_type': 'product',
+#             'og_title': self.object.meta_title,
+#             'og_description': self.object.meta_description,
+#         })
+
+#         return context
+
+#     def get_breadcrumbs(self):
+#         """Get breadcrumbs for auction detail"""
+#         breadcrumbs = super().get_breadcrumbs()
         
-        # Add current auction
-        breadcrumbs.append({
-            'title': self.object.title,
-            'url': None
-        })
+#         # Add Auctions level
+#         breadcrumbs.append({
+#             'title': _('Auctions'),
+#             'url': self.get_language_specific_url('auctions:auction-list')
+#         })
         
-        return breadcrumbs
+#         # Add current auction
+#         breadcrumbs.append({
+#             'title': self.object.title,
+#             'url': None
+#         })
+        
+#         return breadcrumbs
--- auctions/views/category_view.py
+++ auctions/views/category_view.py
@@ -1,6 +1,7 @@
 # views/category_views.py
 from .base_view import BaseListView, BaseDetailView
 from ..models import Category, Auction
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_control
 from django.utils.translation import gettext_lazy as _
@@ -49,100 +50,148 @@ class CategoryListView(BaseListView):
         return breadcrumbs
 
 @method_decorator(cache_control(public=True, max_age=3600), name='dispatch')
-class CategoryDetailView(BaseDetailView):
-    model = Category
+class CategoryDetailView(BaseListView):
+    model = Auction
     template_name = 'auctions/category_detail.html'
-    context_object_name = 'category'
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
+    ordering = 'end_time'
     
-    def get_navigation_objects(self):
-        """Override to sort categories alphabetically by title instead of by date"""
-        try:
-            queryset = self.get_queryset()
-            prev_obj = queryset.filter(
-                title__lt=self.object.title
-            ).order_by('-title').first()
-            next_obj = queryset.filter(
-                title__gt=self.object.title
-            ).order_by('title').first()
-            return prev_obj, next_obj
-        except:
-            return None, None
-
-    def get_related_objects(self, limit=4):
-        """Get active auctions from this category"""
-        try:
-            return Auction.objects.filter(
-                category=self.object,
-                is_active=True
-            ).order_by('-created_at')[:limit]
-        except:
-            return []
-
-    def get_schema_data(self):
-        """Generate Schema.org data for the category"""
-        category = self.object
-        schema = {
-            "@context": "https://schema.org",
-            "@type": "CollectionPage",
-            "name": category.title,
-            "description": category.description or '',
-            "breadcrumb": {
-                "@type": "BreadcrumbList",
-                "itemListElement": [
-                    {
-                        "@type": "ListItem",
-                        "position": 1,
-                        "name": _("Home"),
-                        "item": self.get_language_specific_url('home')
-                    },
-                    {
-                        "@type": "ListItem",
-                        "position": 2,
-                        "name": _("Categories"),
-                        "item": self.get_language_specific_url('auctions:category-list')
-                    },
-                    {
-                        "@type": "ListItem",
-                        "position": 3,
-                        "name": category.title
-                    }
-                ]
-            }
-        }
-        return schema
-
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'], is_active=True)
+        return queryset.filter(category=self.category)
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category = self.object
         
-        # Add category-specific meta tags
         context.update({
-            'meta_title': _('{category_name} Auctions').format(
-                category_name=category.meta_title
-            ),
-            'meta_description': category.meta_description[:160] if category.meta_description else '',
-            'breadcrumbs': [
-                {
-                    'title': _('Home'),
-                    'url': self.get_language_specific_url('home')
-                },
-                {
-                    'title': _('Categories'),
-                    'url': self.get_language_specific_url('auctions:category-list')
-                },
-                {
-                    'title': category.title,
-                    'url': None
-                }
-            ],
-            # Get all active auctions for this category
-            'auctions': Auction.objects.filter(
-                category=category,
-                is_active=True
-            ).order_by('-created_at'),
-            'og_type': 'website'  # Override article type from base
+            'category': self.category,
+            'meta_title': f"{self.category.title} | {_('Auctions')}",
+            'meta_description': _('{category} auctions available').format(category=self.category.title),
+            'active_auctions': self.get_queryset().filter(
+                end_time__gt=timezone.now()
+            ).count()
         })
         
         return context
+        
+    def get_breadcrumbs(self):
+        breadcrumbs = super().get_breadcrumbs()
+        
+        # Add Categories level
+        breadcrumbs.append({
+            'title': _('Categories'),
+            'url': self.get_language_specific_url('auctions:category-list')
+        })
+        
+        # Add current category
+        breadcrumbs.append({
+            'title': self.category.title,
+            'url': None
+        })
+        
+        return breadcrumbs
+
+
+# @method_decorator(cache_control(public=True, max_age=3600), name='dispatch')
+# class CategoryDetailView(BaseDetailView):
+#     model = Category
+#     template_name = 'auctions/category_detail.html'
+#     context_object_name = 'category'
+#     slug_field = 'slug'
+#     slug_url_kwarg = 'slug'
+    
+#     def get_navigation_objects(self):
+#         """Override to sort categories alphabetically by title instead of by date"""
+#         try:
+#             queryset = self.get_queryset()
+#             prev_obj = queryset.filter(
+#                 title__lt=self.object.title
+#             ).order_by('-title').first()
+#             next_obj = queryset.filter(
+#                 title__gt=self.object.title
+#             ).order_by('title').first()
+#             return prev_obj, next_obj
+#         except:
+#             return None, None
+
+#     def get_related_objects(self, limit=4):
+#         """Get active auctions from this category"""
+#         try:
+#             return Auction.objects.filter(
+#                 category=self.object,
+#                 is_active=True
+#             ).order_by('-created_at')[:limit]
+#         except:
+#             return []
+
+#     def get_schema_data(self):
+#         """Generate Schema.org data for the category"""
+#         category = self.object
+#         schema = {
+#             "@context": "https://schema.org",
+#             "@type": "CollectionPage",
+#             "name": category.title,
+#             "description": category.description or '',
+#             "breadcrumb": {
+#                 "@type": "BreadcrumbList",
+#                 "itemListElement": [
+#                     {
+#                         "@type": "ListItem",
+#                         "position": 1,
+#                         "name": _("Home"),
+#                         "item": self.get_language_specific_url('home')
+#                     },
+#                     {
+#                         "@type": "ListItem",
+#                         "position": 2,
+#                         "name": _("Categories"),
+#                         "item": self.get_language_specific_url('auctions:category-list')
+#                     },
+#                     {
+#                         "@type": "ListItem",
+#                         "position": 3,
+#                         "name": category.title
+#                     }
+#                 ]
+#             }
+#         }
+#         return schema
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         category = self.object
+#         active_auctions = Auction.objects.filter(
+#             category=category,
+#             is_active=True,  # The auction must be active
+#         )
+#         # Add category-specific meta tags
+#         context.update({
+#             'meta_title': _('{category_name} Auctions').format(
+#                 category_name=category.meta_title
+#             ),
+#             'meta_description': category.meta_description[:160] if category.meta_description else '',
+#             'breadcrumbs': [
+#                 {
+#                     'title': _('Home'),
+#                     'url': self.get_language_specific_url('home')
+#                 },
+#                 {
+#                     'title': _('Categories'),
+#                     'url': self.get_language_specific_url('auctions:category-list')
+#                 },
+#                 {
+#                     'title': category.title,
+#                     'url': None
+#                 }
+#             ],
+#             # Get all active auctions for this category
+#             'object_list': Auction.objects.filter(
+#                 category=category,
+#                 is_active=True
+#             ).order_by('-created_at'),
+#             'og_type': 'website',  # Override article type from base
+#             'active_auctions': active_auctions.count()
+
+#         })
+        
+#         return context
